a9d9d73bd3d2b01ac06f5af0f2dcf98abc8f3fdf
Dmitry Ovcharenko <ovcharenkodmitry@gmail.com>
2012-11-29 11:44:04 +0400

    deploy.rb удаление лишнего

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,6 @@ set :repository, "git://github.com/ovcharenkodi/people.git"
 # infobox ubuntu server
 server "109.120.166.170", :web, :app, :db, :primary => true
 
-
 ssh_options[:port] = 22
 ssh_options[:keys] = "/home/dmitry/.ssh/id_rsa"
 
@@ -20,26 +19,11 @@ default_run_options[:pty] = true
 set :deploy_to, "/home/#{user}/app/"
 set :use_sudo, false
 
-# set :shell, '/bin/bash'
-# default_run_options[:shell] = '/bin/bash'
-# /home/dmitry/.rvm/gems/ruby-1.9.3-p327-perf/bin/bundle
-
 # Add RVM's lib directory to the load path.
 $:.unshift(File.expand_path('./lib', ENV['rvm_path']))
-
-# Load RVM's capistrano plugin.    
-
+# RVM's capistrano plugin.    
 set :rvm_ruby_string, "1.9.3-p327-perf"
-set :rvm_type, :user  # Don't use system-wide RVM
-
-
-
-
-
-
-
-
-
+set :rvm_type, :user 
 
 set :deploy_via, :remote_cache
 # set :deploy_via, :copy
@@ -48,13 +32,4 @@ set :copy_strategy, :export
 namespace :deploy do
 	task :start do ; end
 	task :stop do ; end
-	
-	# task :t do
-	# 	run "rvm reload"
-	# end
-
 end
-
-
-
-# before "bundle:install", "deploy:t"
