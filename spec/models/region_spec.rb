2f7388cbb82c59c55d768885c265a97a95b8d84a
dmitry <dmitry@server.(none)>
2012-12-06 18:18:35 +0400

    fix text

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -2,31 +2,31 @@ require 'spec_helper'
 
 
 describe Region do
-	before :each do
-		@region = Region.new
-	end
+  before :each do
+    @region = Region.new
+  end
 
-	describe "name" do
-	  it "should have name" do
-	  	@region.should be_respond_to(:name) 
-	  end
+  describe "name" do
+    it "should have name" do
+      @region.should be_respond_to(:name) 
+    end
 
-	  it "should save value name" do
-	    @region.name = "random first name"
-	    @region.name.should == "random first name"
-	  end
-	end
-	
-	describe "id_vk" do
-	  it "should have id_vk" do
-	  	@region.should be_respond_to(:id_vk) 
-	  end
+    it "should save value name" do
+      @region.name = "random first name"
+      @region.name.should == "random first name"
+    end
+  end
 
-	  it "should save value id_vk" do
-	    @region.id_vk = -100046
-	    @region.id_vk.should == -100046
-	  end
-	end
+    describe "id_vk" do
+      it "should have id_vk" do
+        @region.should be_respond_to(:id_vk) 
+      end
+
+      it "should save value id_vk" do
+        @region.id_vk = -100046
+        @region.id_vk.should == -100046
+      end
+    end
 
   describe "valid" do  
     it "should be valid" do
