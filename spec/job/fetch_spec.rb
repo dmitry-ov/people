2c1754ec7f4a51d2bd52608deb57176510b47ab0
Dmitry Ovcharenko <ovcharenkodmitry@gmail.com>
2012-12-29 00:22:23 -0800

    add spec fetch method

---
+++ spec/job/fetch_spec.rb
@@ -0,0 +1,43 @@
+#encoding: utf-8
+require 'spec_helper'
+
+describe Fetch do
+  before :each do
+    @fetch = Fetch.new(Region.first)  
+  end
+  
+  
+  pending "method initialize"
+
+  pending "method perform"
+
+  pending "method success"
+
+  pending "method error"
+
+  pending "method fail"
+
+  # it "should respond config" do
+  #   @load.should be_respond_to(:config)
+  # end
+
+  # it "shold respond config is hash" do
+  #   @load.config.class.to_s.should == "Hash"
+  # end
+
+  # it "should respond make_criterias" do
+  #   @load.should be_respond_to(:make_criterias)
+  # end
+
+  # it "shold respond make_criterias is hash" do
+  #   @load.make_criterias.class.to_s.should == "Array"
+  # end
+
+  # it "shold respond by respond method " do
+  #   @load.respond({:sex=>2, :age=>20}).class.to_s.should == "Hash"
+  # end
+  
+end
+
+
+
