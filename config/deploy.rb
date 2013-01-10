b87d93c1f55e243c8c59f7d0bf5c6d10c5a27b82
Dmitry Ovcharenko <ovcharenkodmitry@gmail.com>
2013-01-10 09:25:20 -0800

    fix ip

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,13 +5,7 @@ set :application, "people"
 set :scm, :git
 set :repository, "git://github.com/ovcharenkodi/people.git"
 
-# 1gb Ubuntu server
-server "81.177.142.175", :web, :app, :db, :primary => true
-# server "10.8.0.6", :web, :app, :db, :primary => true
-
-
-# infobox ubuntu server
-# server "109.120.166.170", :web, :app, :db, :primary => true
+server "94.249.192.123", :web, :app, :db, :primary => true
 
 ssh_options[:port] = 22
 ssh_options[:keys] = "/home/dmitry/.ssh/id_rsa"
