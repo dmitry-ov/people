590663ae7d7adbad1e88710ce742b652bb85224f
Dmitry Ovcharenko <ovcharenkodmitry@gmail.com>
2013-01-21 17:36:39 +0400

    Delayed::Worker.max_run_time = 25.minutes

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -2,7 +2,7 @@
 # Для того что бы упавшую задачу было видно в интерфейсе DJ_Mon, мы не удаляем ее из очереди
 Delayed::Worker.destroy_failed_jobs = true
 
-Delayed::Worker.sleep_delay = 1
+Delayed::Worker.sleep_delay = 3
 Delayed::Worker.max_attempts = 3
 Delayed::Worker.max_run_time = 25.minutes
 Delayed::Worker.read_ahead = 10
